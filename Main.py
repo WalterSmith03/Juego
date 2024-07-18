35e2b2ad569d8c8844435a4f3b8b16ca269ca4e6
WALTER EDUARDO RAPALO SMITH <werapalo@uth.hn>
2024-07-18 16:47:42 -0600

    Commit Clase 18 de Julio

--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
 import pygame
-
+import Constantes
 pygame.init()
 
 ancho = 800
 alto = 600
 
-ventana = pygame.display.set_mode((ancho, alto))
-
-#PRUEBA
+ventana = pygame.display.set_mode((Constantes.ANCHO_VENTANA,
+                                   Constantes.ALTO_VENTANA))
+pygame
+run = True
+while run == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
