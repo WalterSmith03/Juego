5d353447bc19b6ddf53ffc62d98e5852d371b48a
WALTER EDUARDO RAPALO SMITH <werapalo@uth.hn>
2024-07-21 22:55:16 -0600

    Commit 21 de julio
    Creación del cuadrado de colisión del personaje y se comenzó con la codificación para poder mover al personaje.

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 import pygame
 import Constantes
+from Personaje import Personaje
+
+
+Jugador = Personaje(50, 50)
+
 pygame.init()
 
 ancho = 800
@@ -10,9 +15,49 @@ ventana = pygame.display.set_mode((Constantes.ANCHO_VENTANA,
 
 pygame.display.set_caption("Mi Primer Juego")
 
+#DEFINIR LAS VARIABLES DE MOVIMIENTO DEL JUGADOR
+mover_arriba = False
+mover_abajo = False
+mover_izquierda = False
+mover_derecha = False
+
 run = True
 while run == True:
+
+    #CALCULAR EL MOVIMIENTO DEL JUGADOR
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+        delta_x = 5
+    if mover_izquierda == True:
+        delta_x = -5
+    if mover_arriba == True:
+        delta_x = -5
+    if mover_derecha == True:
+        delta_x = 5
+
+
+
+
+
+    Jugador.dibujar(ventana)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                mover_izquierda = True
+            if event.key == pygame.K_d:
+                mover_derecha = True
+            if event.key == pygame.K_w:
+                mover_arriba = True
+            if event.key == pygame.K_s:
+                mover_abajo = True
+
+
+    pygame.display.update()
+
 pygame.quit()
--- Personaje.py
+++ Personaje.py
@@ -1,9 +1,11 @@
 import pygame
+import Constantes
 
 class Personaje():
     def __init__(self, x, y):
-        self.forma = pygame.Rect(0, 0, 20, 20)
+        self.forma = pygame.Rect(0, 0, Constantes.ANCHO_PERSONAJE,
+                                        Constantes.ALTO_PERSONAJE)
         self.forma.center = (x,y)
 
-        def dibujar(self, interfaz):
-            pygame.draw.rect(interfaz, color:(255, 255, 0), self.forma)
+    def dibujar(self, interfaz):
+        pygame.draw.rect(interfaz, Constantes.COLOR_PERSONAJE,  self.forma)
